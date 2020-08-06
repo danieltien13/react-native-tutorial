0b7c5440a1b31b725c9c236bdf20ce083a7ba0aa
Daniel Tien <dtien@Daniels-MacBook-Pro.local>
2020-08-05 23:37:01 -0700

    finished welcome and view image screens

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -19,47 +19,12 @@ import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center", // main
-        alignItems: "center", // secondary
-        alignContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
-      />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  // return <WelcomeScreen></WelcomeScreen>;
+  return <ViewImageScreen></ViewImageScreen>;
 }
 
 const styles = StyleSheet.create({
---
+++ DoneWithIt/app/screens/ViewImageScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcon} />
+      <View style={styles.deleteIcon} />
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      ></Image>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ecdc4",
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
+export default ViewImageScreen;
