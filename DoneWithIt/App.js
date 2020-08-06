4240070d65c50d464fbc5f059f6265b5162a2512
Daniel Tien <dtien@Daniels-MacBook-Pro.local>
2020-08-05 22:56:59 -0700

    begin building welcome screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,14 +1,63 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  View,
+  Image,
+  SafeAreaView,
+  Alert,
+  Button,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
-  console.log("App executed");
-
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center", // main
+        alignItems: "center", // secondary
+        alignContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
+      />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
@@ -17,7 +66,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
