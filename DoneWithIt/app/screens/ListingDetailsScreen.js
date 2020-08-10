0abdafeedbde56fb56f444f7c158f3987a01d10b
Daniel Tien <dtien@Daniels-MacBook-Pro.local>
2020-08-09 18:10:41 -0700

    finished styling

---
+++ DoneWithIt/app/screens/ListingDetailsScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import AppText from "../components/AppText";
+import colors from "../config/colors";
+
+export default function ListingDetailsScreen(props) {
+  return (
+    <View>
+      <Image
+        style={styles.image}
+        source={require("../assets/jacket.jpg")}
+      ></Image>
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+});
--- DoneWithIt/app/screens/ViewImageScreen.js
+++ DoneWithIt/app/screens/ViewImageScreen.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import colors from "../config/colors";
 
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon} />
-      <View style={styles.deleteIcon} />
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" size={35} color="white" />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          size={35}
+          color="white"
+        />
+      </View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -18,17 +28,11 @@ function ViewImageScreen(props) {
 
 const styles = StyleSheet.create({
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
