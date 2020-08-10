995d15575d5992228404be6df7f5c61dee120166
Daniel Tien <dtien@Daniels-MacBook-Pro.local>
2020-08-09 21:58:55 -0700

    planning account screen

--- DoneWithIt/app/components/ListItem.js
+++ DoneWithIt/app/components/ListItem.js
@@ -1,23 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
 import AppText from "./AppText";
 import colors from "../config/colors";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
-export default function ListItem({ title, subTitle, image }) {
+export default function ListItem({
+  title,
+  subTitle,
+  image,
+  onPress,
+  renderRightActions,
+}) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image}></Image>
-      <View>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
-      </View>
-    </View>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={image}></Image>
+          <View>
+            <AppText style={styles.title}>{title}</AppText>
+            <AppText style={styles.subTitle}>{subTitle}</AppText>
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    padding: 15,
   },
   image: {
     width: 70,
--- DoneWithIt/app/screens/MessagesScreen.js
+++ DoneWithIt/app/screens/MessagesScreen.js
@@ -1,8 +1,12 @@
-import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
 import ListItem from "../components/ListItem";
+import Constants from "expo-constants";
+import Screen from "../components/Screen";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: "T1",
@@ -17,19 +21,46 @@ const messages = [
   },
 ];
 
-export default function MessagesScreen() {
+export default function MessagesScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    // Delete the message from messages array
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(message) => message.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.description}
-          image={item.image}
-        ></ListItem>
-      )}
-    ></FlatList>
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log("Message clicked", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction
+                onPress={() => handleDelete(item)}
+              ></ListItemDeleteAction>
+            )}
+          ></ListItem>
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/mosh.jpg"),
+            },
+          ]);
+        }}
+      ></FlatList>
+    </Screen>
   );
 }
 
