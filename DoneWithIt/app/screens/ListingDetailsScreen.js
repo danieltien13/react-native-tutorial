dc31261fac6585af4dd79f3a6ee0e5aa01f4152d
Daniel Tien <dtien@Daniels-MacBook-Pro.local>
2020-08-09 19:34:12 -0700

    extracting the screen component

--- DoneWithIt/app/screens/ListingDetailsScreen.js
+++ DoneWithIt/app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import AppText from "../components/AppText";
 import colors from "../config/colors";
+import ListItem from "../components/ListItem";
 
 export default function ListingDetailsScreen(props) {
   return (
@@ -13,6 +14,13 @@ export default function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Mosh Hamedani"
+            subTitle="5 Listings"
+          ></ListItem>
+        </View>
       </View>
     </View>
   );
@@ -35,5 +43,8 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
     marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
